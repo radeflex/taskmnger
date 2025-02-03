97e4abd3768e4fd2a1016ce24712e4dd86206a05
radeflex <maks.krav61@gmail.com>
2025-02-04 00:21:04 +0300

    added removing button and some code refactoring

--- src/main/java/taskmanager/ui/TaskUIFactory.java
+++ src/main/java/taskmanager/ui/TaskUIFactory.java
@@ -1,21 +1,42 @@
 package taskmanager.ui;
 
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import taskmanager.logic.Task;
+import taskmanager.logic.TaskBoard;
+
+import java.util.Optional;
 
 public class TaskUIFactory {
-    public static VBox createTaskUI(Task task) {
+    public static VBox createTaskUI(Task task, MainViewController controller) {
         VBox vBox = new VBox();
-        Label taskDesc = new Label(task.getDesc());
-        Label dateAdded = new Label("added at: " + task.getDateAdded().toString());
         vBox.setSpacing(10);
         vBox.setPadding(new Insets(20, 20, 20, 20));
-        vBox.getChildren().add(taskDesc);
-        vBox.getChildren().add(dateAdded);
 
+        Label taskDesc = new Label(task.getDesc());
+        Label dateAdded = new Label("added at: " + task.getDateAdded().toString());
+
+        Button removeBtn = new Button("X");
+
+        HBox controlPanel = new HBox();
+        controlPanel.setSpacing(10);
+        controlPanel.getChildren().add(removeBtn);
+
+        removeBtn.setOnAction(e -> {
+            Optional<ButtonType> choice = AlertFactory.createAlert(Alert.AlertType.CONFIRMATION, "Do you really want to remove it?").showAndWait();
+            if (choice.isPresent() && choice.get().equals(ButtonType.OK)) {
+                TaskBoard.remove(task);
+                controller.removeTask(task);
+            }
+        });
+
+        vBox.getChildren().addAll(taskDesc, dateAdded, controlPanel);
         return vBox;
     }
 }
