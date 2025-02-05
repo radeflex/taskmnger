615d16132bbe1d549e4bd96383bc7c749e6ccb30
radeflex <maks.krav61@gmail.com>
2025-02-05 19:58:34 +0300

    a bit changes

--- src/main/java/taskmanager/jdbc/TaskTable.java
+++ src/main/java/taskmanager/jdbc/TaskTable.java
@@ -17,30 +17,30 @@ public class TaskTable {
     private static final String DEL_TASK = "DELETE FROM tasks WHERE name = ?;";
     private static final String GET_ALL_TASKS = "SELECT * FROM tasks";
 
-    public static void add(Task task) {
+    public static void add(String taskDesc, int taskStatus, String taskDate) {
         try (PreparedStatement stmt = DBUtils.getConnection().prepareStatement(ADD_TASK)) {
-            stmt.setString(1, task.getDesc());
-            stmt.setInt(2, task.getStatus().ordinal());
-            stmt.setString(3, task.getDateAdded().toString());
+            stmt.setString(1, taskDesc);
+            stmt.setInt(2, taskStatus);
+            stmt.setString(3, taskDate);
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void update(String taskName, Status status) {
+    public static void update(String taskDesc, int taskStatus) {
         try (PreparedStatement stmt = DBUtils.getConnection().prepareStatement(UPD_TASK)) {
-            stmt.setInt(1, status.ordinal());
-            stmt.setString(2, taskName);
+            stmt.setInt(1, taskStatus);
+            stmt.setString(2, taskDesc);
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void remove(Task task) {
+    public static void remove(String taskName) {
         try (PreparedStatement stmt = DBUtils.getConnection().prepareStatement(DEL_TASK)) {
-            stmt.setString(1, task.getDesc());
+            stmt.setString(1, taskName);
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
