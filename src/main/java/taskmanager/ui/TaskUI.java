ffffbcd072f8af9f7fb3557ccf1f59b28b5462eb
radeflex <maks.krav61@gmail.com>
2025-02-05 20:04:08 +0300

    TaskUI logic was separated

---
+++ src/main/java/taskmanager/ui/TaskUI.java
@@ -0,0 +1,47 @@
+package taskmanager.ui;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import taskmanager.logic.Status;
+import taskmanager.logic.Task;
+
+public class TaskUI extends VBox {
+    private final Button demoteBtn, promoteBtn, removeBtn;
+
+    public TaskUI(Task task) {
+        super();
+
+        setSpacing(10);
+        setPadding(new Insets(20, 20, 20, 20));
+
+        Label dateAdded = new Label("added at: " + task.getDateAdded().toString());
+        Label desc = new Label(task.getDesc());
+
+        demoteBtn = new Button("<-");
+        promoteBtn = new Button("->");
+        removeBtn = new Button("X");
+
+        demoteBtn.setDisable(task.getStatus().equals(Status.TODO));
+        promoteBtn.setDisable(task.getStatus().equals(Status.DONE));
+
+        HBox controlPanel = new HBox(demoteBtn, promoteBtn, removeBtn);
+        controlPanel.setSpacing(10);
+
+        getChildren().addAll(desc, dateAdded, controlPanel);
+    }
+
+    public Button getDemoteBtn() {
+        return demoteBtn;
+    }
+
+    public Button getPromoteBtn() {
+        return promoteBtn;
+    }
+
+    public Button getRemoveBtn() {
+        return removeBtn;
+    }
+}
